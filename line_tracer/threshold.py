b2805b8588b65eaabfcce70933ff251309c475c0
jun <chchdelm3@icloud.com>
2022-07-20 15:44:39 +0900

    update

--- line_tracer/threshold.py
+++ line_tracer/threshold.py
@@ -15,7 +15,7 @@ def main():
         crop_img = frame[60:120, 0:160]
         gray = cv2.cvtColor(crop_img, cv2.COLOR_BGR2GRAY)
         blur = cv2.GaussianBlur(gray, (5, 5), 0)
-        ret, thresh1 = cv2.threshold(blur, 130, 255, cv2.THRESHOLD_BINARY_INV)
+        ret, thresh1 = cv2.threshold(blur, 170, 255, cv2.THRESH_BINARY_INV)
 
         cv2.imshow('thresh1', thresh1)
 
