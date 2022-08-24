902ab5fca91d3119f04d90361829078c4e0f1444
Jun <fohens295@gmail.com>
2022-08-24 14:54:50 +0900

    add model

---
+++ lane_detection/version.py
@@ -0,0 +1,9 @@
+import tensorflow as tf
+from tensorflow import keras
+import numpy as numpy
+import h5py
+
+print("tensorflow: ", tf.__version__)
+print("keras: ", keras.__version__)
+print("numpy: ", numpy.__version__)
+print("h5py: ", h5py.__version__)
